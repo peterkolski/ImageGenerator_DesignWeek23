aec3669f621572d2950790b5108cc5552ddb0adc
Peter Kolski <mail@peterkolski.com>
2023-04-21 16:03:15 +0200

    Empty project

---
+++ DesignWeek23/DesignWeek23/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DesignWeek23
+//
+//  Created by Peter Kolski on 21.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignWeek23/DesignWeek23/DesignWeek23App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignWeek23App.swift
+//  DesignWeek23
+//
+//  Created by Peter Kolski on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct DesignWeek23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
