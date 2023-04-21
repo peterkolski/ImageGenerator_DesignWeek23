368b44a4dea06785b4afd14ba084af75dded5cff
Peter Kolski <mail@peterkolski.com>
2023-04-21 19:29:36 +0200

    Working code to recieve an image

--- DesignWeek23/DesignWeek23/ContentView.swift
+++ DesignWeek23/DesignWeek23/ContentView.swift
@@ -1,138 +1,124 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var userInput: String = ""
-    @State private var image: Image? = nil
-    @State private var errorMessage: String = ""
+    @State private var text = ""
+    @State private var image: UIImage? = nil
+    @State private var errorMessage: String? = nil
     
     var body: some View {
         VStack {
-            TextField("Enter text", text: $userInput)
+            TextField("Enter text", text: $text)
                 .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-            
-            Button(action: {
-                getImage()
-            }) {
-                Text("Get Image")
-            }
             
             if let image = image {
-                image
+                Image(uiImage: image)
                     .resizable()
-                    .scaledToFit()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
             }
             
-            if !errorMessage.isEmpty {
+            if let errorMessage = errorMessage {
                 Text(errorMessage)
                     .foregroundColor(.red)
                     .padding()
             }
+            
+            Button("Generate image") {
+                generateImage(from: text) { result in
+                    switch result {
+                    case .success(let image):
+                        self.image = image
+                        self.errorMessage = nil
+                    case .failure(let error):
+                        self.image = nil
+                        self.errorMessage = error.localizedDescription
+                    }
+                }
+            }
         }
+        .padding()
+    }
+}
+
+func generateImage(from text: String, completion: @escaping (Result<UIImage, Error>) -> Void) {
+    guard let apiKey = Constants.apiKey else {
+        completion(.failure(NSError(domain: "generateImage", code: 0, userInfo: [NSLocalizedDescriptionKey: "API key not set"])))
+        return
+    }
+
+    let urlString = "https://api.stability.ai/v1/generation/\(Constants.engineID)/text-to-image"
+    
+    guard let url = URL(string: urlString) else {
+        completion(.failure(NSError(domain: "generateImage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+        return
     }
     
-    func getImage() {
-        guard let apiKey = Constants.apiKey else {
-            errorMessage = "API key not set"
-            return
-        }
-
-        let urlString = "https://api.stability.ai/v1/generation/\(Constants.engineID)/text-to-image"
-        
-        guard let url = URL(string: urlString) else {
-            errorMessage = "Invalid URL"
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
-        
-        let parameters: [String: Any] = [
-            "cfg_scale": 7,
-            "clip_guidance_preset": "FAST_BLUE",
-            "height": Constants.imageHeight,
-            "width": Constants.imageWidth,
-            "sampler": "K_DPM_2_ANCESTRAL",
-            "samples": 1,
-            "steps": 5,
-            "text_prompts": [
-                [
-                    "text": userInput,
-                    "weight": 1
-                ]
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+    
+    let parameters: [String: Any] = [
+        "cfg_scale": 7,
+        "clip_guidance_preset": "FAST_BLUE",
+        "height": Constants.imageHeight,
+        "width": Constants.imageWidth,
+        "sampler": "K_DPM_2_ANCESTRAL",
+        "samples": 1,
+        "steps": 75,
+        "text_prompts": [
+            [
+                "text": text,
+                "weight": 1
             ]
         ]
-        
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-            errorMessage = "Invalid request parameters"
+    ]
+    
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+        completion(.failure(NSError(domain: "generateImage", code: 2, userInfo: [NSLocalizedDescriptionKey: "Invalid request parameters"])))
+        return
+    }
+    
+    request.httpBody = httpBody
+    
+    URLSession.shared.dataTask(with: request) { (data, response, error) in
+        guard let data = data else {
+            completion(.failure(error ?? NSError(domain: "generateImage", code: 3, userInfo: [NSLocalizedDescriptionKey: "Unknown error"])))
             return
         }
         
-        request.httpBody = httpBody
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {
-                errorMessage = "Error: \(error?.localizedDescription ?? "Unknown error")"
-                return
-            }
-            
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                // Log the response headers
-                let responseHeaders = httpResponse.allHeaderFields
-                print("Response headers: \(responseHeaders)")
+        // Debugging: print raw data
+        print("Raw data:")
+        print(data)
 
-                switch httpResponse.statusCode {
-                case 200:
-                    // Parse the base64-encoded image from the response data
-//                    guard let imageString = String(data: data, encoding: .utf8) else {
-//                        // Handle invalid image string
-//                        print("Error: Invalid image string")
-//                        return
-//                    }
-                    guard let imageData = Data(base64Encoded: data) else {
-                        // Handle invalid base64-encoded image data
-                        print("Error: Invalid base64-encoded image data")
-                        return
-                    }
-                    guard let image = UIImage(data: imageData) else {
-                        // Handle invalid image data
-                        print("Error: Invalid image data")
-                        return
-                    }
-                    
-                    DispatchQueue.main.async {
-                        // Update the image in the UI
-                        self.image = Image(uiImage: image)
-                    }
-                case 400:
-                    errorMessage = "Bad request"
-                case 401:
-                    errorMessage = "Unauthorized"
-                case 403:
-                    errorMessage = "Forbidden"
-                case 404:
-                    errorMessage = "Not found"
-                case 429:
-                    errorMessage = "Too many requests"
-                case 500:
-                    errorMessage = "Internal server error"
-                case 503:
-                    errorMessage = "Service unavailable"
-                default:
-                    errorMessage = "Unknown error (\(httpResponse.statusCode))"
-                }
-            } else {
-                errorMessage = "Invalid server response"
-            }
-        }.resume()
-    }
+        // Parse JSON response
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+            completion(.failure(NSError(domain: "generateImage", code: 4, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON response"])))
+            return
+        }
 
+        // Extract base64-encoded image data from the JSON response
+        guard let artifacts = json["artifacts"] as? [[String: Any]],
+              let imageDataString = artifacts.first?["base64"] as? String,
+              let imageData = Data(base64Encoded: imageDataString) else {
+            completion(.failure(NSError(domain: "generateImage", code: 5, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
+            return
+        }
+
+        // Debugging: print decoded data
+        print("Decoded data:")
+        print(imageData)
+
+        // Convert to UIImage
+        guard let image = UIImage(data: imageData) else {
+            completion(.failure(NSError(domain: "generateImage", code: 6, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
+            return
+        }
+
+        completion(.success(image))
+    }.resume()
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ DesignWeek23/DesignWeek23/ContentView2.swift
@@ -0,0 +1,141 @@
+import SwiftUI
+
+struct ContentView2: View {
+    @State private var userInput: String = ""
+    @State private var image: Image? = nil
+    @State private var errorMessage: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter text", text: $userInput)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+            
+            Button(action: {
+                getImage()
+            }) {
+                Text("Get Image")
+            }
+            
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+        }
+    }
+    
+    func getImage() {
+        guard let apiKey = Constants.apiKey else {
+            errorMessage = "API key not set"
+            return
+        }
+
+        let urlString = "https://api.stability.ai/v1/generation/\(Constants.engineID)/text-to-image"
+        
+        guard let url = URL(string: urlString) else {
+            errorMessage = "Invalid URL"
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+        
+        let parameters: [String: Any] = [
+            "cfg_scale": 7,
+            "clip_guidance_preset": "FAST_BLUE",
+            "height": Constants.imageHeight,
+            "width": Constants.imageWidth,
+            "sampler": "K_DPM_2_ANCESTRAL",
+            "samples": 1,
+            "steps": 5,
+            "text_prompts": [
+                [
+                    "text": userInput,
+                    "weight": 1
+                ]
+            ]
+        ]
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            errorMessage = "Invalid request parameters"
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                errorMessage = "Error: \(error?.localizedDescription ?? "Unknown error")"
+                return
+            }
+            
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                // Log the response headers
+                let responseHeaders = httpResponse.allHeaderFields
+                print("Response headers: \(responseHeaders)")
+
+                switch httpResponse.statusCode {
+                case 200:
+                    // Parse the base64-encoded image from the response data
+//                    guard let imageString = String(data: data, encoding: .utf8) else {
+//                        // Handle invalid image string
+//                        print("Error: Invalid image string")
+//                        return
+//                    }
+                    guard let imageData = Data(base64Encoded: data) else {
+                        // Handle invalid base64-encoded image data
+                        print("Error: Invalid base64-encoded image data")
+                        return
+                    }
+                    guard let image = UIImage(data: imageData) else {
+                        // Handle invalid image data
+                        print("Error: Invalid image data")
+                        return
+                    }
+                    
+                    DispatchQueue.main.async {
+                        // Update the image in the UI
+                        self.image = Image(uiImage: image)
+                    }
+                case 400:
+                    errorMessage = "Bad request"
+                case 401:
+                    errorMessage = "Unauthorized"
+                case 403:
+                    errorMessage = "Forbidden"
+                case 404:
+                    errorMessage = "Not found"
+                case 429:
+                    errorMessage = "Too many requests"
+                case 500:
+                    errorMessage = "Internal server error"
+                case 503:
+                    errorMessage = "Service unavailable"
+                default:
+                    errorMessage = "Unknown error (\(httpResponse.statusCode))"
+                }
+            } else {
+                errorMessage = "Invalid server response"
+            }
+        }.resume()
+    }
+
+}
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
+    }
+}
