5c30f58f33b6f8a4ff5edafa3311c4b9dc50a338
Peter Kolski <mail@peterkolski.com>
2023-04-21 16:48:10 +0200

    New code from ChatGPT4

--- DesignWeek23/DesignWeek23/ContentView.swift
+++ DesignWeek23/DesignWeek23/ContentView.swift
@@ -1,141 +1,107 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var inputText = ""
-    @State private var image: UIImage?
-    @State private var errorMessage = ""
+    @State private var userInput: String = ""
+    @State private var image: Image? = nil
+    @State private var errorMessage: String = ""
     
     var body: some View {
         VStack {
-            TextField("Enter text", text: $inputText)
+            TextField("Enter text", text: $userInput)
                 .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
             
             Button(action: {
-                // Send the input text to the API and update the image
-                self.sendInputToAPI()
+                getImage()
             }) {
-                Text("Submit")
+                Text("Get Image")
             }
             
             if let image = image {
-                Image(uiImage: image)
+                image
                     .resizable()
                     .scaledToFit()
-                    .padding()
             }
             
-            Text(errorMessage)
-                .foregroundColor(.red)
-                .padding()
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
         }
     }
     
-    func sendInputToAPI() {
-        guard let url = URL(string: "https://platform.stability.ai/v1/models/pix2pix/predict") else {
-            // handle invalid URL
+    func getImage() {
+        let apiKey = Constants.apiKey
+        let urlString = "https://platform.stability.ai/rest-api/convert-to-image"
+        
+        guard let url = URL(string: urlString) else {
+            errorMessage = "Invalid URL"
             return
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.setValue("Bearer YOUR_API_KEY", forHTTPHeaderField: "Authorization")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(apiKey, forHTTPHeaderField: "X-API-KEY")
         
-        let json = ["input": inputText]
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-        request.httpBody = jsonData
+        let parameters: [String: Any] = [
+            "text": userInput
+        ]
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                // Handle the error
-                let message = self.messageForErrorCode(error._code)
-                self.errorMessage = "Error: \(message)"
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                // Handle invalid response
-                self.errorMessage = "Error: Invalid response"
-                return
-            }
-            
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            errorMessage = "Invalid request parameters"
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else {
-                // Handle missing data
-                self.errorMessage = "Error: Missing data"
+                errorMessage = "Error: \(error?.localizedDescription ?? "Unknown error")"
                 return
             }
             
-            if response.statusCode == 200 {
-                // Parse the image from the response data
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        // Update the image in the UI
-                        self.image = image
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200:
+                    if let image = UIImage(data: data) {
+                        self.image = Image(uiImage: image)
+                    } else {
+                        errorMessage = "Invalid image data"
                     }
+                case 400:
+                    errorMessage = "Bad request"
+                case 401:
+                    errorMessage = "Unauthorized"
+                case 403:
+                    errorMessage = "Forbidden"
+                case 404:
+                    errorMessage = "Not found"
+                case 429:
+                    errorMessage = "Too many requests"
+                case 500:
+                    errorMessage = "Internal server error"
+                case 503:
+                    errorMessage = "Service unavailable"
+                default:
+                    errorMessage = "Unknown error (\(httpResponse.statusCode))"
                 }
-                
-                // Clear any previous error messages
-                self.errorMessage = ""
             } else {
-                // Handle non-200 status code
-                let message = self.messageForErrorCode(response.statusCode)
-                self.errorMessage = "Error: \(message)"
+                errorMessage = "Invalid server response"
             }
-        }
-        
-        task.resume()
+        }.resume()
     }
-    
-    func messageForErrorCode(_ errorCode: Int) -> String {
-        switch errorCode {
-        case -1001:
-            return "The request timed out"
-        case -1003:
-            return "A URL is malformed"
-        case -1004:
-            return "The resource could not be found"
-        case -1005:
-            return "The network connection was lost"
-        case -1009:
-            return "The internet connection is offline"
-        case -1010:
-            return "The request was not allowed"
-        case -1011:
-            return "The server encountered an error"
-        case -1012:
-            return "The user cancelled the request"
-        case -1013:
-            return "The request was too large"
-        case -1014:
-            return "The server requires authentication"
-        case -1015:
-            return "The server requires a secure connection"
-        case -1100..<0:
-            return "An unknown networking error occurred"
-        case 400:
-            return "The request was malformed or invalid"
-        case 401:
-            return "Authentication failed or user not authorized"
-        case 403:
-            return "The user is not allowed to access the resource"
-        case 404:
-            return "The requested resource was not found"
-        case 405:
-            return "The method is not allowed for the resource"
-        case 500:
-            return "A server error occurred"
-        case 503:
-            return "The service is unavailable"
-        default:
-            print("Unknown error code: \(errorCode)")
-            return "An unknown error occurred"
-        }
-    }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Constants {
+    static let apiKey = "sk-zUcWdOuTDjZR3gURbarc49AI7YOS5DMNk8b07V9g5v8hkVGl"
 }
