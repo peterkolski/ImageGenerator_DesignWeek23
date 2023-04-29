7bc3656037a56d72f64b3e2485eb00f587659cee
Peter Kolski <mail@peterkolski.com>
2023-04-29 17:36:23 +0200

    Separated into a class

--- DesignWeek23/DesignWeek23/ContentView.swift
+++ DesignWeek23/DesignWeek23/ContentView.swift
@@ -1,10 +1,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var text = ""
-    @State private var image: UIImage? = UIImage(named: "SampleImage")
-    @State private var errorMessage: String? = nil
     @State private var isFullScreen = false
+    @StateObject var viewModel = ImageGeneratorModel()
     
     var body: some View {
             ZStack{
@@ -20,7 +18,7 @@ struct ContentView: View {
                         VStack{
                             if !isFullScreen{
                                 // Text input field
-                                TextField("Enter text", text: $text)
+                                TextField("Enter text", text: $viewModel.text)
                                     .padding()
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 8)
@@ -28,7 +26,7 @@ struct ContentView: View {
                                     )
                                 
                                 // Display any error message
-                                if let errorMessage = errorMessage {
+                                if let errorMessage = viewModel.errorMessage {
                                     Text(errorMessage)
                                         .foregroundColor(.red)
                                         .padding()
@@ -36,14 +34,14 @@ struct ContentView: View {
                                 
                                 // Button to generate the image
                                 Button("Generate image") {
-                                    generateImage(from: text) { result in
+                                    viewModel.generateImage { result in
                                         switch result {
                                         case .success(let image):
-                                            self.image = image
-                                            self.errorMessage = nil
+                                            viewModel.image = image
+                                            viewModel.errorMessage = nil
                                         case .failure(let error):
-                                            self.image = nil
-                                            self.errorMessage = error.localizedDescription
+                                            viewModel.image = nil
+                                            viewModel.errorMessage = error.localizedDescription
                                         }
                                     }
                                 }
@@ -56,7 +54,7 @@ struct ContentView: View {
                         }
                         VStack{
                             // Display the generated image
-                            if let image = image {
+                            if let image = viewModel.image {
                                 Image(uiImage: image)
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
@@ -84,88 +82,95 @@ struct ContentView: View {
 }
 
 
+// ------------------
 
-
-
-func generateImage(from text: String, completion: @escaping (Result<UIImage, Error>) -> Void) {
-    guard let apiKey = Constants.apiKey else {
-        completion(.failure(NSError(domain: "generateImage", code: 0, userInfo: [NSLocalizedDescriptionKey: "API key not set"])))
-        return
-    }
+class ImageGeneratorModel: ObservableObject {
+    @Published var text = ""
+    @Published var image: UIImage? = nil
+    @Published var errorMessage: String? = nil
+    @Published var isLoading = false
     
-    let urlString = "https://api.stability.ai/v1/generation/\(Constants.engineID)/text-to-image"
-    
-    guard let url = URL(string: urlString) else {
-        completion(.failure(NSError(domain: "generateImage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-        return
-    }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue(apiKey, forHTTPHeaderField: "Authorization")
-    
-    let parameters: [String: Any] = [
-        "cfg_scale": 7,
-        "clip_guidance_preset": "FAST_BLUE",
-        "height": Constants.imageHeight,
-        "width": Constants.imageWidth,
-        "sampler": "K_DPM_2_ANCESTRAL",
-        "samples": 1,
-        "steps": 20,
-        "text_prompts": [
-            [
-                "text": text,
-                "weight": 20
+    func generateImage(completion: @escaping (Result<UIImage, Error>) -> Void) {
+        guard let apiKey = Constants.apiKey else {
+            completion(.failure(NSError(domain: "generateImage", code: 0, userInfo: [NSLocalizedDescriptionKey: "API key not set"])))
+            return
+        }
+        
+        let urlString = "https://api.stability.ai/v1/generation/\(Constants.engineID)/text-to-image"
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "generateImage", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+        
+        let parameters: [String: Any] = [
+            "cfg_scale": 7,
+            "clip_guidance_preset": "FAST_BLUE",
+            "height": Constants.imageHeight,
+            "width": Constants.imageWidth,
+            "sampler": "K_DPM_2_ANCESTRAL",
+            "samples": 1,
+            "steps": 20,
+            "text_prompts": [
+                [
+                    "text": text,
+                    "weight": 20
+                ]
             ]
         ]
-    ]
-    
-    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
-        completion(.failure(NSError(domain: "generateImage", code: 2, userInfo: [NSLocalizedDescriptionKey: "Invalid request parameters"])))
-        return
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            completion(.failure(NSError(domain: "generateImage", code: 2, userInfo: [NSLocalizedDescriptionKey: "Invalid request parameters"])))
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                completion(.failure(error ?? NSError(domain: "generateImage", code: 3, userInfo: [NSLocalizedDescriptionKey: "Unknown error"])))
+                return
+            }
+            
+            // Debugging: print raw data
+            print("Raw data:")
+            print(data)
+            
+            // Parse JSON response
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                completion(.failure(NSError(domain: "generateImage", code: 4, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON response"])))
+                return
+            }
+            
+            // Extract base64-encoded image data from the JSON response
+            guard let artifacts = json["artifacts"] as? [[String: Any]],
+                  let imageDataString = artifacts.first?["base64"] as? String,
+                  let imageData = Data(base64Encoded: imageDataString) else {
+                completion(.failure(NSError(domain: "generateImage", code: 5, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
+                return
+            }
+            
+            // Debugging: print decoded data
+            print("Decoded data:")
+            print(imageData)
+            
+            // Convert to UIImage
+            guard let image = UIImage(data: imageData) else {
+                completion(.failure(NSError(domain: "generateImage", code: 6, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
+                return
+            }
+            
+            completion(.success(image))
+        }.resume()
     }
-    
-    request.httpBody = httpBody
-    
-    URLSession.shared.dataTask(with: request) { (data, response, error) in
-        guard let data = data else {
-            completion(.failure(error ?? NSError(domain: "generateImage", code: 3, userInfo: [NSLocalizedDescriptionKey: "Unknown error"])))
-            return
-        }
-        
-        // Debugging: print raw data
-        print("Raw data:")
-        print(data)
-        
-        // Parse JSON response
-        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
-            completion(.failure(NSError(domain: "generateImage", code: 4, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON response"])))
-            return
-        }
-        
-        // Extract base64-encoded image data from the JSON response
-        guard let artifacts = json["artifacts"] as? [[String: Any]],
-              let imageDataString = artifacts.first?["base64"] as? String,
-              let imageData = Data(base64Encoded: imageDataString) else {
-            completion(.failure(NSError(domain: "generateImage", code: 5, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
-            return
-        }
-        
-        // Debugging: print decoded data
-        print("Decoded data:")
-        print(imageData)
-        
-        // Convert to UIImage
-        guard let image = UIImage(data: imageData) else {
-            completion(.failure(NSError(domain: "generateImage", code: 6, userInfo: [NSLocalizedDescriptionKey: "Invalid image data"])))
-            return
-        }
-        
-        completion(.success(image))
-    }.resume()
 }
 
+// -----------------
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ DesignWeek23/DesignWeek23/ImageGeneratorModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageGeneratorModel.swift
+//  DesignWeek23
+//
+//  Created by Peter Kolski on 29.04.23.
+//
+
+import Foundation
